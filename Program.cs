3c4d19793aaa51fcec0f4ecfa2bbf146ac3c8ff8
maljaethereal <121117752+MaksimOlienyk@users.noreply.github.com>
2025-09-28 18:33:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Interface_segregation_principle
+{
+interface IMagician
+    {
+        void Cast();
+    }
+
+    interface ISwordsman
+    {
+        void Attack();
+    }
+
+    class MasterOfAll : IMagician, ISwordsman
+    {
+        public void Cast() => Console.WriteLine("Кастую реквієм");
+        public void Attack() => Console.WriteLine("Атакую мечем");
+    }
+
+    class Witch : IMagician
+    {
+        public void Cast() => Console.WriteLine("Відьмую відьмовське");
+    }
+}
